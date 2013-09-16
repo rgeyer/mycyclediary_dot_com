58e6b58a3214538562d9011107dee3e366ffffed
Ryan J. Geyer <me@ryangeyer.com>
2013-09-16 16:43:54 -0700

    Heirarchy changes

--- mycyclediary_dot_com/settings/common.py
+++ mycyclediary_dot_com/settings/common.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/ryangreyer/Code/Python/mycyclediary_dot_com/mycyclediary_dot_com_sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'mycyclediary_dot_com_project.urls'
+ROOT_URLCONF = 'mycyclediary_dot_com.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'mycyclediary_dot_com_project.wsgi.application'
+WSGI_APPLICATION = 'mycyclediary_dot_com.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -120,10 +120,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mycyclediary_dot_com_www',
-    'mycyclediary_dot_com_api',
+    'mycyclediary_dot_com.apps.api',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- mycyclediary_dot_com/urls.py
+++ mycyclediary_dot_com/urls.py
@@ -6,12 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mycyclediary_dot_com.views.home', name='home'),
+    url(r'^$', 'mycyclediary_dot_com.views.index', name='index'),
     # url(r'^mycyclediary_dot_com/', include('mycyclediary_dot_com.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
