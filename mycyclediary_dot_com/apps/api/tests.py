6a2c9ea52ad2cc5867a77ce5f9ae129b0e4d5c86
Ryan J. Geyer <me@ryangeyer.com>
2013-09-16 17:46:21 -0700

    Dirty hacky addition of fitparse, will move this around

--- mycyclediary_dot_com/apps/api/tests.py
+++ mycyclediary_dot_com/apps/api/tests.py
@@ -5,12 +5,41 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'vendor', 'python-fitparse'))
+from fitparse import Activity
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class FitParseTests(TestCase):
+    def test_can_parse_activity(self):
+        activity = Activity("/Users/ryangreyer/Downloads/FitSDKRelease_8.00/examples/Activity.fit")
+        activity.parse()
+
+        # Records of type 'record' (I know, confusing) are the entries in an
+        # activity file that represent actual data points in your workout.
+        records = activity.get_records_by_type('record')
+        current_record_number = 0
+
+        for record in records:
+
+            # Print record number
+            current_record_number += 1
+            print (" Record #%d " % current_record_number).center(40, '-')
+
+            # Get the list of valid fields on this record
+            valid_field_names = record.get_valid_field_names()
+
+            for field_name in valid_field_names:
+                # Get the data and units for the field
+                field_data = record.get_data(field_name)
+                field_units = record.get_units(field_name)
+
+                # Print what we've got!
+                if field_units:
+                    print " * %s: %s %s" % (field_name, field_data, field_units)
+                else:
+                    print " * %s: %s" % (field_name, field_data)
+
+            print
